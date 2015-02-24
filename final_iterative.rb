aa27f9c13a92d7309148ae906bcc5ab9f87361b9
ycartc <tracymcaruso@gmail.com>
2015-02-24 13:06:26 -0700

    Final Linked

---
+++ final_iterative.rb
@@ -0,0 +1,118 @@
+class IterativeLinkedList
+  attr_accessor :head
+
+  def initialize
+    @head = nil
+  end
+
+  def count
+    if @head.nil?
+      0
+    else
+      count = 1
+      current = @head
+      while current.link?
+        count += 1
+        current = current.link
+      end
+      count
+    end
+  end
+
+  def push(data)
+    if !@head
+      @head = Node.new(data)
+    else
+      current = head
+      while current.link?
+        current = current.link
+      end
+      current.link = Node.new(data)
+    end
+  end
+
+  def last_node
+    current = head
+    while current.link?
+      current = current.link
+    end
+    current
+  end
+
+  def pop
+    current = head
+    previous = nil
+    while current.link?
+      previous = current
+      current = current.link
+    end
+
+    if previous.nil?
+      @head = nil
+    else
+      previous.link = nil
+    end
+    current.data
+  end
+
+
+  def delete(target)
+    if head.data == target
+      @head = head.link
+    else
+      current = head
+      last = nil
+      while current.link?
+        last = current
+        current = current.link
+
+        if current.data == target
+          last.link = current.link
+        end
+      end
+    end
+  end
+
+  def to_a
+    result = []
+    current = head
+
+    while current
+      result << current.data
+      current = current.link
+    end
+    return result
+  end
+
+
+end
+
+
+
+
+class Node
+  attr_accessor :link
+  attr_reader :data
+
+  def initialize(input_data)
+    @data = input_data
+  end
+
+  def link?
+    link
+  end
+end
+
+
+
+list = IterativeLinkedList.new
+
+list.push("Cat")
+list.push("Dog")
+list.push("Pizza")
+list.push("Cat")
+list.push("Dog")
+
+
+
+puts list.inspect.split
---
+++ final_recursive.rb
@@ -0,0 +1,58 @@
+class LinkedList
+  attr_reader :head
+
+
+  def push(data, node = head)
+    if node.nil?
+      @head = Node.new(data)
+    else
+      if node.next_node.nil?
+        node.next_node = Node.new(data)
+      else
+          push(data, node.next_node)
+      end
+    end
+  end
+
+
+  def count(node = head)
+    if node.nil?
+      0
+    else
+      1 + count(node.next_node)
+    end
+  end
+
+
+  def position(index, node = head)
+    return nil unless node
+      if index == 0
+        node.data
+      else
+        position(index - 1, node.next_node)
+      end
+    end
+  end
+
+  def pop(node = head)
+    if node.nil?
+      @head = nil
+    elsif node.next_node && node.next_node.next_node.nil?
+      node.next_node = nil
+    else
+      pop(node.next_node)
+    end
+  end
+
+
+end
+
+
+
+class Node
+  attr_accessor :next_node, :data
+
+  def initialize(data)
+    @data = data
+  end
+end
